184bd8ea69dc3bee0651b91646ce44bb41406894
Tolero2 <110334793+Tolero2@users.noreply.github.com>
2023-04-24 15:42:22 +0200

    update

--- MySQLConnector_v1/MySQLPy.py
+++ MySQLConnector_v1/MySQLPy.py
@@ -4,51 +4,31 @@ from mysql.connector import connect, Error
 config ={
     'host' : '127.0.0.1',
     'user': input('Enter Username: '),
-    'password': getpass(),
+    'password': getpass('Enter password: '),
 }
-DB_NAME = "online_movie_rating6"
-try:
-    with connect(
-        **config
-    ) as connection:
-       createDBQuery= "CREATE DATABASE {}".format(DB_NAME)
-       with connection.cursor() as cursor:
-        #cursor.execute(createDBQuery)
-        connection.database= DB_NAME
-except Error as e:
-        print("Error: {}. check input details".format(e.msg ))
-else:
-    print("Successfully connected to MySQL Sever!")  
-finally:
-    with connect(
-        **config
-    ) as connection:
-       showDBQuery = "SHOW DATABASES"
-       with connection.cursor() as cursor:
-        cursor.execute(showDBQuery)
-        result = cursor.fetchall()
-        for db in result:
-             print(db)
+DB_NAME = "online_movie_rating10"
 
-    TABLES = {}
+TABLES = {}
 
-    TABLES['movies']= ("""
-    CREATE TABLE `movies`(
+TABLES['movies']= ("""
+    CREATE TABLE movies(
     `id` INT NOT NULL AUTO_INCREMENT,
     `title` NVARCHAR(50) NULL,
     `release_year` DATE NULL,
     `genre` NVARCHAR(50) NULL,
-    `collection_in_mil` float NULL,
-    PRIMARY KEY (`id`))""")
+    `collection_in_mil` DECIMAL(4,2) NULL,
+    PRIMARY KEY (`id`)
+    )
+    """)
 
-    TABLES['reviewers'] = ("""
+TABLES['reviewers'] = ("""
     CREATE TABLE reviewers (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `first_name` VARCHAR(100),
     `last_name` VARCHAR(100)
 )
 """)
-    TABLES['ratings'] = ("""
+TABLES['ratings'] = ("""
     CREATE TABLE ratings (
     movie_id INT,
     reviewer_id INT,
@@ -56,21 +36,78 @@ finally:
     FOREIGN KEY(movie_id) REFERENCES movies(id),
     FOREIGN KEY(reviewer_id) REFERENCES reviewers(id),
     PRIMARY KEY(movie_id, reviewer_id)
+    )
     """)
-    try:
-       with connect(
+
+  ## Define a DBConn_Exec function Automatic open/close DB connection and execute (MySQL QUERY) passed as a parameter and return the fetch all as the results if any.
+
+
+def DBConn_Exec (query):
+        with connect(
         **config
-    ) as connection:
-          #create a loop for the insert tables, put the connection into a function; returning cursor/ or csrExc
-          createTableQuery= "{}".format("".join(TABLES['movies']))
-          with connection.cursor() as cursor:
+        ) as connection:
             connection.database= DB_NAME
-            cursor.execute(createTableQuery)
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                curResults = cursor.fetchall()
+                connection.commit()
+                return curResults
+##Database Queries to excute using the DBConn_Exec function.
+
+useDBQuery= "USE {}".format(DB_NAME)
+createDBQuery= "CREATE DATABASE {}".format(DB_NAME)
+try:
+
+        useDBQuery= "USE {}".format(DB_NAME)
+        with connect(
+        **config
+        ) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(useDBQuery)
+except Error as e:
+        print("Database Error: {}.\nCreating {} database Now!".format(e.msg, DB_NAME))
+        createDBQuery= "CREATE DATABASE {}".format(DB_NAME)
+        with connect(
+        **config
+        ) as connection:
+            print("Successfully connected to MySQL sever!")
+            with connection.cursor() as cursor:
+                cursor.execute(createDBQuery)
+                print("Database created")
+            #   cursorConn = conn.cursor()
+            #   cursorConn
+else:
+    print("Successfully connected to MySQL sever and database!")
+
+finally:
+
+
+    try:
+          #create a loop for the insert tables, put the connection into a function; returning cursor/ or csrExc
+        showDBQuery = "SHOW DATABASES"
+        cursorResults = DBConn_Exec(showDBQuery)
+        # for cursorShowDB in cursorResults:
+        #      for db in cursorShowDB:
+        #         print(db)
+        for cursorShowDB in cursorResults:
+             #for db in cursorShowDB:
+                print(cursorShowDB)
+
+        #cursor.execute(createTableQuery)
+        for table in TABLES:
+            print("Creating {} table".format(table))
+            createTableQuery= "{}".format(TABLES[f'{table}'])
+            cursorTable = DBConn_Exec(createTableQuery)
+
+
+        #with connection.cursor() as cursor:
+            #connection.database= DB_NAME
+        #cursor.execute(createTableQuery)
     except Error as e:
-        print("Error: {}. check input details".format(e.msg ))
+        print("Error 2: {}. check input details".format(e ))
     else:
-        print("Successfully connected to MySQL Sever!")  
-print("Connection closed!")
+        print("Successfully created table ")
+print("Sever connection closed!")
 
 
 #import mySQLConnectPy
