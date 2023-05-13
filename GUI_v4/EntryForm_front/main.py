4e462e349b20c1d85facd368307520722b80ee22
Tolero2 <110334793+Tolero2@users.noreply.github.com>
2023-05-13 05:56:07 +0200

    update

--- GUI_v4/EntryForm_front/main.py
+++ GUI_v4/EntryForm_front/main.py
@@ -12,49 +12,54 @@ frame=Frame(window,bg=bg)
 frame.pack()
 
 #Configure Label frames to group different data logic
-userLabelFrame=LabelFrame(frame,bg=bg,text="User Information")
-userLabelFrame.grid(row=0,column=0)
-registrationLabelFrame=LabelFrame(frame,bg=bg)
-registrationLabelFrame.grid(row=1,column=0)
-t_cLabelFrame=LabelFrame(frame,bg=bg,text="Terms & Conditions")
-t_cLabelFrame.grid(row=2,column=0)
+user_labelFrame=LabelFrame(frame,bg=bg,text="User Information")
+user_labelFrame.grid(row=0,column=0,sticky="news")
+registration_labelFrame=LabelFrame(frame,bg=bg)
+registration_labelFrame.grid(row=1,column=0,sticky="news",pady=15)
+termsConditions_labelFrame=LabelFrame(frame,bg=bg,text="Terms & Conditions")
+termsConditions_labelFrame.grid(row=2,column=0,sticky="news")
 
 #Add WIDGETS to respective Label Frames
 
 #User Information frame WIDGETS
 #Labels
-firstName_label=Label(userLabelFrame,bg=bg,text="First Name")
+firstName_label=Label(user_labelFrame,bg=bg,text="First Name")
 firstName_label.grid(row=0,column=0)
-lastName_label=Label(userLabelFrame,bg=bg,text="Last Name")
+lastName_label=Label(user_labelFrame,bg=bg,text="Last Name")
 lastName_label.grid(row=0,column=1)
-title_label=Label(userLabelFrame,bg=bg,text="Title")
+title_label=Label(user_labelFrame,bg=bg,text="Title")
 title_label.grid(row=0,column=2)
-age_label=Label(userLabelFrame,bg=bg,text="Age")
+age_label=Label(user_labelFrame,bg=bg,text="Age")
 age_label.grid(row=2,column=0)
-nationality_label=Label(userLabelFrame,bg=bg,text="Nationality")
+nationality_label=Label(user_labelFrame,bg=bg,text="Nationality")
 nationality_label.grid(row=2,column=1)
 
 #Entries
-firstName_entry=Entry(userLabelFrame,bg="#FFFFFF")
+firstName_entry=Entry(user_labelFrame,bg=bg)
 firstName_entry.grid(row=1,column=0)
-lastName_entry=Entry(userLabelFrame,bg="#FFFFFF")
+lastName_entry=Entry(user_labelFrame,bg=fg,insertbackground=bg,fg=bg)
 lastName_entry.grid(row=1,column=1)
 
 #Registration frame WIDGETS
 #Labels
-registrationStat_label=Label(registrationLabelFrame,bg=bg,text="Registration Status")
+registrationStat_label=Label(registration_labelFrame,bg=bg,text="Registration Status")
 registrationStat_label.grid(row=0,column=0)
-courses_label=Label(registrationLabelFrame,bg=bg,text="# Completed Courses")
+courses_label=Label(registration_labelFrame,bg=bg,text="# Completed Courses")
 courses_label.grid(row=0,column=1)
-semester_label=Label(registrationLabelFrame,bg=bg,text="# Semesters")
+semester_label=Label(registration_labelFrame,bg=bg,text="# Semesters")
 semester_label.grid(row=0,column=2)
 
 #Check Buttons
-registration_checkbox=Checkbutton(registrationLabelFrame,bg=bg,text="Currently Registered")
+registration_checkbox=Checkbutton(registration_labelFrame,bg=bg,text="Currently Registered")
 registration_checkbox.grid(row=1,column=0)
 
 #DropDown Buttons
 courses_dropdown=""
+
+#Terms & Conditions frame WIDGETS
+#Check Buttons
+termsConditions_checkbox=Checkbutton(termsConditions_labelFrame,bg=bg,text="I accept the terms and conditions.")
+termsConditions_checkbox.grid(row=0,column=0)
 
 
 
