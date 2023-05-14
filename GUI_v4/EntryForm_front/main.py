eefa2108ef71dfde5c562ba7ada95bafea13193b
Tolero2 <110334793+Tolero2@users.noreply.github.com>
2023-05-14 19:49:14 +0200

    update

--- GUI_v4/EntryForm_front/main.py
+++ GUI_v4/EntryForm_front/main.py
@@ -1,7 +1,18 @@
-#Import evrything from tkinter
+#Import evrything from tkinter // import ttk wrapper
+from ast import Str
 from tkinter import *
-bg="#333333"
+from tkinter import ttk
+
+#define general Background and Foreground color
+bg="grey"
 fg="#FFFFFF"
+
+#Define enter data function that will get executed when the button is clicked,
+def enterData():
+    firstName=firstnameVar.get()
+    lastName=lastnameVar.get()
+    print(firstName,lastName)
+
 window=Tk()
 window.title("TKINTER DATA ENTRY FORM")
 window.geometry("500x500")
@@ -12,35 +23,51 @@ frame=Frame(window,bg=bg)
 frame.pack()
 
 #Configure Label frames to group different data logic
-user_labelFrame=LabelFrame(frame,bg=bg,text="User Information")
-user_labelFrame.grid(row=0,column=0,sticky="news")
+userInfo_labelFrame=LabelFrame(frame,bg=bg,text="User Information")
+userInfo_labelFrame.grid(row=0,column=0)
 registration_labelFrame=LabelFrame(frame,bg=bg)
-registration_labelFrame.grid(row=1,column=0,sticky="news",pady=15)
+registration_labelFrame.grid(row=1,column=0)
 termsConditions_labelFrame=LabelFrame(frame,bg=bg,text="Terms & Conditions")
-termsConditions_labelFrame.grid(row=2,column=0,sticky="news")
+termsConditions_labelFrame.grid(row=2,column=0)
 
-#Add WIDGETS to respective Label Frames
 
-#User Information frame WIDGETS
+##Add all WIDGETS to respective Label Frames
+
+
+#User Information L-frame WIDGETS
 #Labels
-firstName_label=Label(user_labelFrame,bg=bg,text="First Name")
+firstName_label=Label(userInfo_labelFrame,bg=bg,text="First Name")
 firstName_label.grid(row=0,column=0)
-lastName_label=Label(user_labelFrame,bg=bg,text="Last Name")
+lastName_label=Label(userInfo_labelFrame,bg=bg,text="Last Name")
 lastName_label.grid(row=0,column=1)
-title_label=Label(user_labelFrame,bg=bg,text="Title")
+title_label=Label(userInfo_labelFrame,bg=bg,text="Title")
 title_label.grid(row=0,column=2)
-age_label=Label(user_labelFrame,bg=bg,text="Age")
+age_label=Label(userInfo_labelFrame,bg=bg,text="Age")
 age_label.grid(row=2,column=0)
-nationality_label=Label(user_labelFrame,bg=bg,text="Nationality")
+nationality_label=Label(userInfo_labelFrame,bg=bg,text="Nationality")
 nationality_label.grid(row=2,column=1)
 
 #Entries
-firstName_entry=Entry(user_labelFrame,bg=bg)
+firstnameVar=StringVar()
+firstName_entry=Entry(userInfo_labelFrame,textvariable=firstnameVar,bg=bg)
 firstName_entry.grid(row=1,column=0)
-lastName_entry=Entry(user_labelFrame,bg=fg,insertbackground=bg,fg=bg)
+lastnameVar=StringVar()
+lastName_entry=Entry(userInfo_labelFrame,textvariable=lastnameVar,bg=fg,insertbackground=bg,fg=bg)
 lastName_entry.grid(row=1,column=1)
 
-#Registration frame WIDGETS
+#ComboBox // using Themed tkinter(ttk) wrapper
+title_combobox=ttk.Combobox(userInfo_labelFrame,background=bg,values=("","Mr.","Mrs.","Ms."))
+title_combobox.grid(row=1,column=2)
+nationality_combobox=ttk.Combobox(userInfo_labelFrame,background=bg,values="")
+nationality_combobox.grid(row=3,column=1)
+
+#SpinBox
+age_spinbox=Spinbox(userInfo_labelFrame,bg=bg,from_=18,to=110)
+age_spinbox.grid(row=3,column=0)
+
+
+#Registration L-frame WIDGETS
+
 #Labels
 registrationStat_label=Label(registration_labelFrame,bg=bg,text="Registration Status")
 registrationStat_label.grid(row=0,column=0)
@@ -53,14 +80,34 @@ semester_label.grid(row=0,column=2)
 registration_checkbox=Checkbutton(registration_labelFrame,bg=bg,text="Currently Registered")
 registration_checkbox.grid(row=1,column=0)
 
-#DropDown Buttons
-courses_dropdown=""
+#SpinBox
+courses_spinbox=Spinbox(registration_labelFrame,bg=bg,from_=0,to='infinity')
+courses_spinbox.grid(row=1,column=1)
+semester_spinbox=Spinbox(registration_labelFrame,bg=bg,to=8)
+semester_spinbox.grid(row=1,column=2)
 
-#Terms & Conditions frame WIDGETS
+
+#Terms & Conditions L-frame WIDGETS
 #Check Buttons
 termsConditions_checkbox=Checkbutton(termsConditions_labelFrame,bg=bg,text="I accept the terms and conditions.")
 termsConditions_checkbox.grid(row=0,column=0)
 
+#Add data button frame WIDGET
+
+#Button
+enterData_button=Button(frame,text="Enter Data",bg=bg,height=2,command=enterData)
+
+##Add general configuration to the widgets and Frame labels
+
+#Configure addition grid parameters on the Window  Label Frames
+for frames in frame.winfo_children():
+    frames.grid_configure(padx=20,pady=10,sticky="news")
+
+
+#Configure addition grid parameters on the Label frame's widgets
+for widgets in (userInfo_labelFrame.winfo_children(),registration_labelFrame.winfo_children(),termsConditions_labelFrame.winfo_children()):
+    for widget in widgets:
+        widget.grid_configure(padx=10,pady=5)
 
 
 window.mainloop()
